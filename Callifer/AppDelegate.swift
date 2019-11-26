1b14761364329d00a920468eb9e0128da88795d6
rrrad <rrad@mail.ru>
2019-11-26 21:11:05 +0300

    Add metods push notification to voip call

--- Callifer/AppDelegate.swift
+++ Callifer/AppDelegate.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import PushKit
+import CallKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
     var window: UIWindow?
+    let voipPush = PKPushRegistry.init(queue: DispatchQueue.main)
     let callManager = CallManager()
     var providerDelegate: ProviderDelegate!
     var contactAuth: Bool = false
@@ -28,6 +31,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             } 
         }
         
+        voipPush.delegate = self
+        voipPush.desiredPushTypes = [.voIP]
+        
         UNUserNotificationCenter.current().requestAuthorization(options: .badge) { (_, _) in
             
         }
@@ -43,14 +49,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func displayIncommingCall(UUID: UUID,
-                              handele: String,
-                              hasVideo: Bool,
-                              complition: ((Error?) -> Void)?
-    ) { providerDelegate.reportIncomingCall(UUID: UUID,
-                                            handele: handele,
-                                            hasVideo: hasVideo,
-                                            complition: complition)
+    
+    // MARK: PushKit metods
+    
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        // здесь передаются данные изменения состояния на сервер
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        if type == .voIP {
+            // берем информацию из push notification playload
+            if let uuidString = payload.dictionaryPayload["UUID"] as? String,
+            let handle = payload.dictionaryPayload["handle"] as? String,
+            let hasVideo = payload.dictionaryPayload["hasVideo"] as? Bool,
+            let uuid = UUID(uuidString: uuidString) {
+                
+                providerDelegate.reportIncomingCall(UUID: uuid, handele: handle, hasVideo: hasVideo) { (err) in
+                    // errors??
+                }
+            }
+        }
     }
 
     // MARK: UISceneSession Lifecycle
